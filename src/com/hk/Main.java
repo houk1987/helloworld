9877f7198edba0de7f1e0e256b524633e454ca06
xuxiongli <598470593@qq.com>
2020-12-17 08:18:36 +0800

    adsdad

---
+++ src/com/hk/Main.java
@@ -0,0 +1,13 @@
+package com.hk;
+
+public class Main {
+
+    public static void main(String[] args) {
+        say();
+    }
+
+    public static final void say(){
+        Say say = new Person();
+        say.say();
+    }
+}
